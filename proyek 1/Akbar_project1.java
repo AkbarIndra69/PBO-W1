3a0bf4d8b4a9974d6b51d33a36465c804ae75e01
AkbarIndra69 <akbar.indra.saputra@students.untidar.ac.id>
2024-08-27 08:34:04 +0700

    Projek Java 1

---
+++ proyek 1/Akbar_project1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.akbar;
+
+/**
+ *
+ * @author AKBAR
+ */
+
+public class Akbar_project1 {
+    public static void main(String[] args) {
+        String Nama = "Akbar Indra Saputra";
+        String NPM = "2310506015";
+        String Alamat = "Kalinegoro";
+        String NoHp = "089673296374";
+        
+        System.out.println("Nama   : " + Nama);
+        System.out.println("NPM    : " + NPM);
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("No HP  : " + NoHp);
+    }
+}
